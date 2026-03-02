e20be4bbf0737083d25960e523f5045f9d2897ba
thimmireddydheeraj@gmail.com <your_github_email@example.com>
2026-03-02 21:43:30 +0530

    UC-2

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,9 +1,21 @@
+/**
+* 00PSBannerApp UC-2 - 00PS Banner Display Application
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 1.0
+*/
 
-
-
-public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
-}
+	public class OOPSBannerApp {
+		public static void main(String[] args) {
+			System.out.println("   *** " + " " + "     *** " + " " + "   ****** " + " " + "       ***** ");
+        	System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + "   **     ** ");
+        	System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + " ** ");
+        	System.out.println(" **   ** " + " " + " **   ** " + " " + " ****** " + " " + "      ***** ");
+        	System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "               ** ");
+        	System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "     **      ** ");
+        	System.out.println("   *** " + " " + "     *** " + " " + "   ** " + " " + "        ***** ");
+		}
+	}
