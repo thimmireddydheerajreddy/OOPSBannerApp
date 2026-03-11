b491714ce8cac4bff0f056b32b720f012c6e0060
thimmireddydheeraj@gmail.com <your_github_email@example.com>
2026-03-11 13:41:05 +0530

    UC7

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,52 +1,101 @@
+/*
+*OOPSBannerApp UC7
+*
+*@author Dheeraj 
+*@version 7.0
+*/
 
-    public static String[] getOPattern() {
-        return new String[] {
-            " ***** ",
-            "*     *",
-            "*     *",
-            "*     *",
-            " ***** "
-        };
-    }
+public class OOPSBannerApp {
 
-   
-    public static String[] getPPattern() {
-        return new String[] {
-            " ***** ",
-            "*     *",
-            " ***** ",
-            "*      ",
-            "*      "
-        };
-    }
+    static class CharacterPatternMap {
+        private Character character;
+        private String[] pattern;
 
-    
-    public static String[] getSPattern() {
-        return new String[] {
-            " ***** ",
-            "*      ",
-            " ***** ",
-            "      *",
-            " ***** "
-        };
-    }
-
-    
-    public static void main(String[] args) {
-
-       
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
-
-       
-        for (int i = 0; i < oPattern.length; i++) {
-            System.out.println(
-                oPattern[i] + "  " +
-                oPattern[i] + "  " +
-                pPattern[i] + "  " +
-                sPattern[i]
-            );
+        public CharacterPatternMap(Character character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
         }
+
+        public Character getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static CharacterPatternMap[] createCharacterPatternMaps() {
+        CharacterPatternMap[] charMaps = new CharacterPatternMap[4];
+
+        charMaps[0] = new CharacterPatternMap('O', new String[]{
+            "   ***   " ,
+            " **   ** " ,
+            " **   ** " ,
+            " **   ** " ,
+            " **   ** " ,
+            " **   ** " ,
+            "   ***   "
+        });
+
+        charMaps[1] = new CharacterPatternMap('P', new String[]{
+            " ******   " ,
+            " **    ** " ,
+            " **    ** " ,
+            " ******   " ,
+            " **       " ,
+            " **       " ,
+            " **       " 
+        });
+
+        charMaps[2] = new CharacterPatternMap('S', new String[]{
+            "   *****     " ,
+            "  **    **   " ,
+            " **          " ,
+            "   *****     " ,
+            "         **  " ,
+            " **     **   " ,
+            "   *****     " 
+        });
+
+        charMaps[3] = new CharacterPatternMap(' ', new String[]{
+            "    ",
+            "    ",
+            "    ",
+            "    ",
+            "    ",
+            "    ",
+            "    "
+        });
+
+        return charMaps;
+    }
+
+    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
+        for (CharacterPatternMap map : charMaps) {
+            if (map.getCharacter() == Character.toUpperCase(ch)) {
+                return map.getPattern();
+            }
+        }
+        return getCharacterPattern(' ', charMaps);
+    }
+
+    public static void printMessage(String message, CharacterPatternMap[] charMaps) {
+        int lines = 7;
+
+        for (int i = 0; i < lines; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                String[] pattern = getCharacterPattern(ch, charMaps);
+                sb.append(pattern[i]).append(" ");
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        CharacterPatternMap[] charMaps = createCharacterPatternMaps();
+        String message = "OOPS";
+        printMessage(message, charMaps);
     }
 }
